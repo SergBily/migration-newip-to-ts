723c0083569a50bce97b274c61863dff7374bad1
SergBily <sergBily.dev@gmail.com>
2022-07-02 12:36:55 +0300

    refactor: set webpack, ts and eslint configs for project

--- migration-newip-to-ts/webpack.config.js
+++ migration-newip-to-ts/webpack.config.js
@@ -1,32 +1,42 @@
+/* eslint-disable */
 const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
-            },
+        },
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader'
+        }
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, '../dist'),
+        clean: true,
+        assetModuleFilename: '[name][ext]'
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
-        new CleanWebpackPlugin(),
+      new CleanWebpackPlugin(),
+      new EslingPlugin({ extensions: 'ts' }),
     ],
 };
 
