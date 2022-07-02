90acb070c1f69c8c572300b3531c99202f4a7baf
SergBily <sergBily.dev@gmail.com>
2022-07-02 09:54:38 +0300

    feat: add apiIKey in appLoader

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '1857fd51b06641b09728ed545d64c24e', // получите свой ключ https://newsapi.org/
         });
     }
 }
