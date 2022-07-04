ed24010ffba90cca726d716526e5aeda3548bccb
SergBily <sergBily.dev@gmail.com>
2022-07-04 22:23:46 +0300

    refactor: change controller.js to controller.ts file and add type

---
+++ migration-newip-to-ts/src/components/controller/controller.ts
@@ -0,0 +1,46 @@
+import AppLoader from './appLoader';
+import { ResponseDataNews, ResponseDataSources } from '../view/appView';
+
+enum Endpoints {
+    sources = 'sources',
+    everything = 'everything',
+}
+
+class AppController extends AppLoader {
+    public getSources(callback: (data: ResponseDataSources) => void) {
+        super.getResp<ResponseDataSources>(
+            {
+                endpoint: Endpoints.sources,
+            },
+            callback
+        );
+    }
+
+    public getNews(e: MouseEvent, callback: (data: ResponseDataNews) => void) {
+        let target = e.target as HTMLSpanElement;
+        const newsContainer = e.currentTarget as HTMLDivElement;
+
+        while (target !== newsContainer) {
+            if (target.classList.contains('source__item')) {
+                const sourceId = target.getAttribute('data-source-id') as string;
+
+                if (newsContainer.getAttribute('data-source') !== sourceId) {
+                    newsContainer.setAttribute('data-source', sourceId);
+                    super.getResp<ResponseDataNews>(
+                        {
+                            endpoint: Endpoints.everything,
+                            options: {
+                                sources: sourceId,
+                            },
+                        },
+                        callback
+                    );
+                }
+                return;
+            }
+            target = target.parentNode as HTMLDivElement;
+        }
+    }
+}
+
+export default AppController;
